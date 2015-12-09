7c0541eaabde76cda3b5e23cdacb3010c1907a12
Rowan Harry <rowanharry03@gmail.com>
2015-12-09 15:28:56 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
-
-$( document ).ready(function() {
+$(document).ready(function()    {
+	console.log("This is running") // To see if this is actually working in browser console log
 
 	// Adding and removing the flip function with a delay
-	$(".panel").mouseenter(function() {            
+	$(".card").hover(function() {     
+		
+		console.log("Hover activate") // To see if this is actually working in browser console log
+		
 	    var self = $(this);    
-	
+
 	    self.addClass('flip');
 	
 	    window.setTimeout(function() {
